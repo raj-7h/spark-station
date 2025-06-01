6d81702568cdbd1c1a7906c6e97a1f1a0d38f255
raj-7h <rajjha.study@gmail.com>
2025-06-01 17:28:13 +0530

    feat: update server CORS, frontend axios config, and fix .gitignore for node_modules

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,30 @@ dotenv.config();
 
 const app = express();
 
+// Cors configuration
+const VERCEL_FRONTEND_URL = "https://spark-station.vercel.app";
+const LOCAL_FRONTEND_URL = "http://localhost:5173";
+
+const allowedOrigins = [VERCEL_FRONTEND_URL, LOCAL_FRONTEND_URL];
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, Postman, curl)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `The CORS policy for this site does not allow access from the specified Origin: ${origin}`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true, // If you need to handle cookies or authorization headers
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Specify allowed methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+};
+
+app.use(cors(corsOptions));
+
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
